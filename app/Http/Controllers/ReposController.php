b0115d60a235003351b378a18128191fa35f24ab
Roniel <roniel44@gmail.com>
2022-05-31 22:14:16 +0800

    Add upvote action in repos controller.

--- app/Http/Controllers/ReposController.php
+++ app/Http/Controllers/ReposController.php
@@ -5,10 +5,29 @@ namespace App\Http\Controllers;
 use App\Services\GithubReposService;
 use Illuminate\Http\Request;
 
+
 class ReposController extends Controller
 {
+    /**
+     * @param GithubReposService $githubReposService
+     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function index(GithubReposService $githubReposService)
     {
         return $githubReposService->getAll();
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function upvote(Request $request) {
+
+        return json_decode(
+            [
+                'repo_id' => $request->get('repo_id'),
+            ]
+        );
     }
 }
